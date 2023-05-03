30bbe7a9802af5c7fe8dd6a65825fa65a40d21f9
jedddg <38686545+jedddg@users.noreply.github.com>
2023-05-03 13:25:44 +1000

    security and performance changes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const helmet = require("helmet");
+const compression = require("compression");
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 app.use(express.static(__dirname + "/public"));
+
+// Security changes
+app.use(helmet());
+app.disable("x-powered-by");
+
+// Performance changes
+app.use(compression());
 
 app.get("/", (req, res) => {
     res.render("index");
