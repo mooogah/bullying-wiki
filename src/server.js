ccda810f38e67a76f43c0050cfb7a47109689be4
jedddg <38686545+jedddg@users.noreply.github.com>
2023-05-03 12:59:00 +1000

    fix silly mistake

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
